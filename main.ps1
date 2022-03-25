b23371015ab8457f3bfc921eb9804a302b629370
blakeha <blakeha@lsretail.com>
2022-03-25 07:44:32 -0500

    - added parallel functionality - added PS 7 Req -additional documentation

--- main.ps1
+++ main.ps1
@@ -4,28 +4,9 @@ $SCRIPT_VERSION = "1.0.0"
 	
 	function Log($message)
 	{
-		Write-Output "$("[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)) $($message)"
+		Write-Output "$("[{0:MM/dd/yy} {0:HH:mm:ss}][System]" -f (Get-Date)) $($message)"
 	}
 	
-	#Accepts a Job as a parameter and writes the latest progress of it
-	function WriteJobProgress
-	{
-		param($Job)
-	
-		#Make sure the first child job exists
-		if($Job.ChildJobs[0].Progress -ne $null)
-		{
-			#Extracts the latest progress of the job and writes the progress
-			$jobProgressHistory = $Job.ChildJobs[0].Progress;
-			$latestProgress = $jobProgressHistory[$jobProgressHistory.Count - 1];
-			$latestPercentComplete = $latestProgress | Select-Object -expand PercentComplete;
-			$latestActivity = $latestProgress | Select-Object -expand Activity;
-			$latestStatus = $latestProgress | Select-Object -expand StatusDescription;
-		
-			#When adding multiple progress bars, a unique ID must be provided. Here I am providing the JobID as this
-			Write-Progress -Id $Job.Id -Activity $latestActivity -Status $latestStatus -PercentComplete $latestPercentComplete;
-		}
-	}
 	
 	# Get-NetFirewallRule -Name 'WINRM*' | Select-Object Name
 	# Enable-PSRemoting -SkipNetworkProfileCheck -Force
@@ -34,48 +15,53 @@ $SCRIPT_VERSION = "1.0.0"
 	
 	$PROJECT_FILE = ($PSScriptRoot + "\settings.json");
 	$ProjectFile = Get-Content -Path $PROJECT_FILE | ConvertFrom-Json
-	
+	$Root = $PSScriptRoot
 	$jobs = [System.Collections.ArrayList]@();
 	
 	# loop machines in project file
-	foreach ($server in $ProjectFile.servers)
+	$ProjectFile.servers | ForEach-Object -Parallel {
+        $PROJECT_FILE = $using:PROJECT_FILE
+        $ProjectFile = $using:ProjectFile
+        $Root = $using:Root
+
+        function Log($message)
 	{
-		
-		Write-Output "`n`n#### $($server.hostname) ####`n"
-		
+		Write-Output "$("[{0:MM/dd/yy} {0:HH:mm:ss}][System]" -f (Get-Date)) $($message)"
+	}
+
 		# set machine has trusted
-		set-item wsman:\localhost\client\trustedhosts -Concatenate -value $server.hostname -Force -Confirm:$false
+		#set-item wsman:\localhost\client\trustedhosts -Concatenate -value $_.hostname -Force -Confirm:$false
 		
 		# get credentials for remote access to server
-		if ($server.credentials.password)
+		if ($_.credentials.password)
 		{
-			$securePassword = (ConvertTo-SecureString $server.credentials.password -AsPlainText -Force)
-			$credentials = New-Object System.Management.Automation.PSCredential $server.credentials.username, $securePassword
+			$securePassword = (ConvertTo-SecureString $_.credentials.password -AsPlainText -Force)
+			$credentials = New-Object System.Management.Automation.PSCredential $_.credentials.username, $securePassword
 		}
 		else
 		{
-			$credentials = (Get-Credential -Message "Credentials for $($server.hostname)" -UserName $server.credentials.username)
+			$credentials = (Get-Credential -Message "Credentials for $($_.hostname)" -UserName $_.credentials.username)
 		}
 		
 		try
 		{
-			$session = New-PSSession -ComputerName $server.hostname -Credential $credentials
-			$session
+			$session = New-PSSession -ComputerName $_.hostname -Credential $credentials
 		}
 		catch
 		{
-			Log "There was an issue remoting into machine: $($server.hostname)";
+			Log "There was an issue remoting into machine: $($_.hostname)";
 			Log $_
 		}
 		
 		try
 		{
+            Log "$($_.hostname) transferring bin file"
 			# download package files
 			Invoke-Command -Session $session -ScriptBlock {
 				New-Item -Path 'C:\LS Retail\Multi-Machine_PowerShell\bin' -ItemType Directory -Force -Verbose:$false | Out-Null
 			}
 			
-			Copy-Item -Path "$($PSScriptRoot)\bin\*" -Destination "C:\LS Retail\Multi-Machine_PowerShell\bin\" -ToSession $session
+			Copy-Item -Path "$($Root)\bin\*" -Destination "C:\LS Retail\Multi-Machine_PowerShell\bin\" -ToSession $session | Out-Null
 		}
 		catch
 		{
@@ -83,22 +69,18 @@ $SCRIPT_VERSION = "1.0.0"
 			Log $_
 		}
 		
-		$jobs.Add((Invoke-Command -Session $session -FilePath "$($ProjectFile.script_path)" -AsJob -ArgumentList $ProjectFile, $server))
+		Invoke-Command -Session $session -FilePath "$($ProjectFile.script_path)" -ArgumentList $ProjectFile, $_ 
 		
 		#$j = Get-Job
 		#$j | Format-List -Property *
 		
-		Write-Output "`n`n#### $($server.hostname) End ####"
+		Log "$($_.hostname) connection and transfer complete."
 	}
 	
 	Write-Output "`n`n Waiting for all jobs to complete. Please Hold"
-	
-	while((Get-Job | Where-Object {$_.State -ne "Completed"}).Count -gt 0){
-		foreach ($job in $jobs) {
-			Write-Progress($job);
-		}
-	}
 
 	$res = $jobs | Wait-Job | Receive-Job
 	$res | Out-File -FilePath ".\Logs.txt"
 	$res
+
+Get-PSSession | Disconnect-PSSession | Out-Null
---
+++ readme.txt
@@ -0,0 +1,5 @@
+This script requires powershell 7 to run.
+This script reqires any servers listed in the settings .json file to have remote powershell enabled.
+You can enable remote powershell across all machines via group policy.
+
+Change settings file to point at any script and it will be executed remotely on all machines listed in the servers section.
